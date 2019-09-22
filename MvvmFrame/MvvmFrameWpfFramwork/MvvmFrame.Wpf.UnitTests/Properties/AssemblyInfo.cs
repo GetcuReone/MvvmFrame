2a1727c2ea33244092e2965c8b210ba7ca7df35e
Zhuel Itua <ituazhuel@hotmail.com>
2019-09-22 15:20:26 +0300

    (dev-3) restore MvvmFrame.Wpf.UnitTests project

---
+++ MvvmFrame/MvvmFrameWpfFramwork/MvvmFrame.Wpf.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MvvmFrame.Wpf.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MvvmFrame.Wpf.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0932c40a-4583-405c-8c80-4572e8e0c93a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
