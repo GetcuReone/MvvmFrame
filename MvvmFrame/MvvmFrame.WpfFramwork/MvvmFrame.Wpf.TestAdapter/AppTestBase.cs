9dd886a8a7ada7b466b148a458c3fb5090ef5f47
Zhuel Itua <ituazhuel@hotmail.com>
2019-09-28 12:54:01 +0300

    (dev-3) started working on a project to facilitate library testing

---
+++ MvvmFrame/MvvmFrame.WpfFramwork/MvvmFrame.Wpf.TestAdapter/AppTestBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmFrame.Wpf.TestAdapter
+{
+    /// <summary>
+    /// base class for testing applications written on the MvvmFrame.Wpf
+    /// </summary>
+    public abstract class AppTestBase
+    {
+        private 
+    }
+}
---
+++ MvvmFrame/MvvmFrame.WpfFramwork/MvvmFrame.Wpf.TestAdapter/TestWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MvvmFrame.Wpf.TestAdapter
+{
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    internal partial class TestWindow : Window
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
